86f37e31902fee2314a4e9e4e98fed14474fa03a
Marcus Hert Da Coregio <marcusdacoregio@gmail.com>
2022-01-18 15:40:22 -0300

    Initial commit, straight from start.spring.io

---
+++ src/main/java/com/eazzycode/springsecurityjwtfilter/SpringSecurityJwtFilterApplication.java
@@ -0,0 +1,13 @@
+package com.eazzycode.springsecurityjwtfilter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityJwtFilterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityJwtFilterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eazzycode/springsecurityjwtfilter/SpringSecurityJwtFilterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eazzycode.springsecurityjwtfilter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityJwtFilterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
